f398cc34608c69acb0af889690aedc582afee1f4
Pranav.k <70883488+PrawnThegr8@users.noreply.github.com>
2023-07-09 22:44:06 +0530

    Update StockMarketPredictor0.2.py

--- StockMarketPredictor0.2.py
+++ StockMarketPredictor0.2.py
@@ -1,4 +1,3 @@
-# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
 import yfinance as yf
@@ -7,59 +6,60 @@ from prophet.plot import plot_plotly
 import plotly.graph_objs as go
 import pandas as pd
 
-
-
 START = "2021-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Market Predictor')
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'AMZN', 'RELIANCE.NS')
-selected_stock = st.text_input('Select a stock ticker for prediction(refer YAHOO FINANCE for tickers)')
+selected_stock = st.text_input('Select a stock ticker for prediction (refer YAHOO FINANCE for tickers)')
 
 n_years = st.slider('How many years into the future?', 1, 4)
 period = n_years * 365
 
 @st.cache_data
-
 def load_data(ticker):
-    if selected_stock:  
+    if selected_stock:
         data = yf.download(ticker, START, TODAY)
         data.reset_index(inplace=True)
-        
         return data
+
 if selected_stock:
     data_load_state = st.text('Loading data...')
     data = load_data(selected_stock)
-
     data_load_state.text('Loading data... done!')
-    
-    st.subheader('Raw data')
-    st.write(data.tail())
 
-# Apply rolling average and interpolation to the data
-rolling_window = st.slider('Moving Average(increase for smoother graph)', 1, 30)
-if selected_stock:
-    data['Close_rolling'] = data['Close'].rolling(rolling_window, min_periods=1).mean()
-    data['Close_rolling'] = data['Close_rolling'].interpolate(method='linear')
-    
-# Plot raw data with rolling average
+    # Apply exponential smoothing to the data
+    smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.1, 0.95, 0.9, 0.05)
+
+# Convert 'Date' column to datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+
+# Set 'Date' column as index
+    data.set_index('Date', inplace=True)
+
+# Resample the data to daily frequency
+    daily_data = data.resample('D').interpolate()
+
+# Apply exponential smoothing to the data
+    daily_data['Close_rolling'] = daily_data['Close'].ewm(alpha=1 - smoothing_factor).mean()
+
+# Plot raw data with exponential smoothing
 def plot_raw_data():
     if selected_stock:
         fig = go.Figure()
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="Stock Open"))
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="Stock Close"))
-        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close_rolling'], name=f"Close ({rolling_window}D Rolling Avg)"))
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Open'], name="Stock Open"))
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close'], name="Stock Close"))
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
         fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
         st.plotly_chart(fig)
 if selected_stock:
     plot_raw_data()
 
 # Predict forecast with Prophet
-if selected_stock:
-    df_train = data[['Date', 'Close_rolling']].rename(columns={"Date": "ds", "Close_rolling": "y"})
+    df_train = daily_data[['Close_rolling']].reset_index().rename(columns={"Date": "ds", "Close_rolling": "y"})
 
-    m = Prophet(changepoint_prior_scale=0.05)
+    m = Prophet(changepoint_prior_scale=0.90)  # Adjust the value as needed
     m.fit(df_train)
 
     future = m.make_future_dataframe(periods=period, freq='D')
@@ -77,12 +77,12 @@ if selected_stock:
     st.subheader('Forecast Components')
     fig2 = m.plot_components(forecast)
     st.write(fig2)
+
 footer = """
 <style>
 .footer {
-
     left: 0;
-    bottom:;
+    bottom: 0;
     width: 100%;
     background-color: white;
     color: black;
@@ -91,7 +91,7 @@ footer = """
 </style>
 <div class="footer">
     <p>Coded by Pranav, Ideas by Emil</p>
-    <p>This app is made for educational purposes only. Data it provides is not 100% accurate. </p>
+    <p>This app is made for educational purposes only. Data it provides is not 100% accurate.</p>
     <p>Analyze stocks before investing.</p>
 </div>
 """
