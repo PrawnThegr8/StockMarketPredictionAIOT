f7d13e9b084167a16c3f6d0cc5687e91d339ca5a
Pranav.k <70883488+PrawnThegr8@users.noreply.github.com>
2023-07-28 18:12:01 +0530

    Update StockMarketPredictor0.2.py

--- StockMarketPredictor0.2.py
+++ StockMarketPredictor0.2.py
@@ -33,8 +33,8 @@ if selected_stock:
     data_load_state.text('Loading data... done!')
     
     # Apply exponential smoothing to the data
-    smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.01, 0.095, 0.09, 0.005)
-    changepoint_prior_scale = st.slider('Flexibility of Trend', 0.1, 10.0, 0.5, 0.1, format="%.1f")
+    smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.1, 0.95, 0.9, 0.05)
+    changepoint_prior_scale = st.slider('Flexibility of Trend', 0.01, 1.0, 0.05, 0.01, format="%.1f")
 
     # Convert 'Date' column to datetime format
     data['Date'] = pd.to_datetime(data['Date'])
