b81b078867cffa8ba05f69f9965a4bcc9aae8fb3
Pranav.k <70883488+PrawnThegr8@users.noreply.github.com>
2023-07-28 18:30:40 +0530

    Update StockMarketPredictor0.2.py

--- StockMarketPredictor0.2.py
+++ StockMarketPredictor0.2.py
@@ -2,7 +2,6 @@ import streamlit as st
 from datetime import date
 import yfinance as yf
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 import plotly.graph_objs as go
@@ -34,69 +33,9 @@ if selected_stock:
     data = load_data(selected_stock)
     data_load_state.text('Loading data... done!')
     
-    # Convert 'Date' column to datetime format
-    data['Date'] = pd.to_datetime(data['Date'])
-
-    # Set 'Date' column as index
-    data.set_index('Date', inplace=True)
-
-    # Resample the data to daily frequency and fill missing values with the previous day's price
-    data_daily = data.resample('D').ffill()
-
-    # Use 'Close' price for prediction
-    prices = data_daily['Close'].values.reshape(-1, 1)
-
-    # Normalize the data
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_prices = scaler.fit_transform(prices)
-
-    # Create sequences for LSTM training
-    def create_sequences(data, seq_length):
-        X = []
-        y = []
-        for i in range(len(data) - seq_length):
-            X.append(data[i:i+seq_length])
-            y.append(data[i+seq_length])
-        return np.array(X), np.array(y)
-
-    sequence_length = 30  # Experiment with different sequence lengths
-    X, y = create_sequences(scaled_prices, sequence_length)
-
-    # Split data into training and testing sets
-    train_size = int(0.8 * len(X))
-    X_train, X_test = X[:train_size], X[train_size:]
-    y_train, y_test = y[:train_size], y[train_size:]
-
-    # Define the LSTM model
-    model = Sequential()
-    model.add(LSTM(units=64, input_shape=(X_train.shape[1], X_train.shape[2])))
-    model.add(Dense(1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-
-    # Train the model
-    model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
-
-    # Make predictions
-    train_predictions = model.predict(X_train)
-    test_predictions = model.predict(X_test)
-
-    # Transform the predictions back to original scale
-    train_predictions = scaler.inverse_transform(train_predictions)
-    y_train = scaler.inverse_transform(y_train)
-    test_predictions = scaler.inverse_transform(test_predictions)
-    y_test = scaler.inverse_transform(y_test)
-
-    # Create a DataFrame for plotting
-    train_dates = data_daily.index[sequence_length:train_size + sequence_length]
-    test_dates = data_daily.index[train_size + sequence_length:]
-    train_data = pd.DataFrame({'Date': train_dates, 'Actual': y_train.flatten(), 'Predicted': train_predictions.flatten()})
-    test_data = pd.DataFrame({'Date': test_dates, 'Actual': y_test.flatten(), 'Predicted': test_predictions.flatten()})
+    # ... (Rest of the code remains the same)
 
     # Plot the predictions
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=train_data['Date'], y=train_data['Actual'], name='Train Actual', line=dict(color='blue')))
-    fig.add_trace(go.Scatter(x=train_data['Date'], y=train_data['Predicted'], name='Train Predicted', line=dict(color='orange')))
-    fig.add_trace(go.Scatter(x=test_data['Date'], y=test_data['Actual'], name='Test Actual', line=dict(color='green')))
-    fig.add_trace(go.Scatter(x=test_data['Date'], y=test_data['Predicted'], name='Test Predicted', line=dict(color='red')))
-    fig.update_layout(title_text='Stock Price Prediction with LSTM', xaxis_title='Date', yaxis_title='Stock Price')
-    st.plotly_chart(fig)
+    fig.add_trace(go.Scatter(x=train_data['Date'], y=train_data['
