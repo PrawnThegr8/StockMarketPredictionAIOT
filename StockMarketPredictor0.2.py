980f79c28c3df442820e41f9e7493ccfc434a7b8
Pranav.k <70883488+PrawnThegr8@users.noreply.github.com>
2023-07-28 19:24:30 +0530

    Update StockMarketPredictor0.2.py

--- StockMarketPredictor0.2.py
+++ StockMarketPredictor0.2.py
@@ -10,7 +10,7 @@ import requests
 from transformers import pipeline
 
 TODAY = date.today().strftime("%Y-%m-%d")
-NEWS_API_KEY = 'd924dd3c445d430ba37bd28e3cd69e32'  # Replace with your News API key
+NEWS_API_KEY = 'YOUR_NEWS_API_KEY'  # Replace with your News API key
 
 st.title('Stock Market Predictor')
 
@@ -53,7 +53,33 @@ if selected_stock:
     smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.1, 0.95, 0.9, 0.05)
     changepoint_prior_scale = st.slider('Flexibility of Trend', 0.1, 10.0, 0.5, 0.1, format="%.1f")
 
-    # ... (Remaining code remains the same)
+    # Convert 'Date' column to datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    # Set 'Date' column as index
+    data.set_index('Date', inplace=True)
+
+    # Resample the data to daily frequency
+    daily_data = data.resample('D').interpolate()
+
+    # Apply exponential smoothing to the data
+    daily_data['Close_rolling'] = daily_data['Close'].ewm(alpha=1 - smoothing_factor).mean()
+
+    # Plot raw data with exponential smoothing
+    def plot_raw_data():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Open'], name="Stock Open"))
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close'], name="Stock Close"))
+        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
+        fig.update_layout(
+            title_text='Stock History',
+            xaxis_rangeslider_visible=True,
+            height=600,  # Set the desired height for the raw data plot
+            width=900  # Set the desired width for the raw data plot
+        )
+        st.plotly_chart(fig)
+
+    plot_raw_data()
 
     # Fetch news for the selected stock
     news_data = get_news(selected_stock)
@@ -84,7 +110,29 @@ if selected_stock:
     else:
         st.subheader("Overall Sentiment: Neutral")
 
+    # Predict forecast with Prophet
+    df_train = daily_data[['Close_rolling']].reset_index().rename(columns={"Date": "ds", "Close_rolling": "y"})
+
+    m = Prophet(
+        growth='linear',
+        changepoint_prior_scale=changepoint_prior_scale
+    )
+
+    # Add additional regressors if available
+    # m.add_regressor('regressor1')
+    # m.add_regressor('regressor2')
+    # ...
+
+    m.fit(df_train)
+
+    future = m.make_future_dataframe(periods=period, freq='D')
+
+    # Fine-tune seasonality parameters if needed
+    # m.add_seasonality(name='weekly', period=7, fourier_order=3, prior_scale=0.1)
+    # m.add_seasonality(name='year
     # ... (Remaining code remains the same)
+
+    forecast = m.predict(future)
 
     # Show and plot forecast
     if n_years == 1:
@@ -94,7 +142,30 @@ if selected_stock:
 
     fig1 = plot_plotly(m, forecast)
 
-    # ... (Remaining code remains the same)
+    # Customize the forecast line appearance
+    fig1.update_traces(mode='lines', line=dict(color='blue', width=2), selector=dict(name='yhat'))
+
+    # Calculate the marker size based on the number of data points
+    num_data_points = len(forecast)
+    marker_size = max(4, 200 // num_data_points)  # Adjust the factor as needed
+
+    # Update the scatter trace to match the forecast more closely
+    fig1.update_traces(mode='markers+lines', marker=dict(size=marker_size, color='black', opacity=0.7),
+                       selector=dict(name='yhat_lower,yhat_upper'))
+
+    fig1.update_layout(
+        title_text=f'Forecast Plot for {n_years} Years',
+        xaxis_rangeslider_visible=True,
+        height=600,  # Set the desired height for the forecast plot
+        width=900,  # Set the desired width for the forecast plot
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        )
+    )
 
     st.plotly_chart(fig1)
 
