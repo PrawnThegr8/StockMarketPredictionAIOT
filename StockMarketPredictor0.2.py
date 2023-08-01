91f836e4d0696e825f4092ef99782cebe981ff6f
Pranav.k <70883488+PrawnThegr8@users.noreply.github.com>
2023-08-01 19:56:56 +0530

    Update StockMarketPredictor0.2.py

--- StockMarketPredictor0.2.py
+++ StockMarketPredictor0.2.py
@@ -49,142 +49,138 @@ negative_words = ['bad', 'poor', 'negative', 'decline', 'failure', 'down', 'loss
 
 st.title('Stock Market Predictor')
 
-selected_stock = st.text_input('Select a stock ticker for prediction (refer to yfinance for ticker)')
+menu = ["Predict Single Stock", "Compare Stocks"]
+choice = st.sidebar.selectbox("Select page", menu)
 
-start_year = st.slider('Select the start year for prediction', 2010, date.today().year - 1, 2020)
-start_date = date(start_year, 1, 1).strftime("%Y-%m-%d")
+if choice == "Predict Single Stock":
+    selected_stock = st.text_input('Select a stock ticker for prediction (refer to yfinance for ticker)')
+    start_year = st.slider('Select the start year for prediction', 2010, date.today().year - 1, 2020)
+    start_date = date(start_year, 1, 1).strftime("%Y-%m-%d")
+    n_years = st.slider('How many years into the future?', 1, 4)
+    period = n_years * 365
 
-n_years = st.slider('How many years into the future?', 1, 4)
-period = n_years * 365
-
-daily_data = None
-
-@st.cache_data
-def load_data(ticker):
     if selected_stock:
-        data = yf.download(ticker, start_date, TODAY)
-        data.reset_index(inplace=True)
-        return data
+        data_load_state = st.text('Loading data...')
+        data = load_data(selected_stock)
+        data_load_state.text('Loading data... done!')
 
-@st.cache_data
-def get_news(stock):
-    if NEWS_API_KEY:
-        company_name = yf.Ticker(stock).info['longName']
-        search_query = f'{stock} OR {company_name}'
-        url = f'https://newsapi.org/v2/everything?q={search_query}&apiKey={NEWS_API_KEY}&pageSize=5'
-        response = requests.get(url)
-        news_data = response.json()
-        return news_data
+        smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.1, 0.95, 0.9, 0.05)
+        changepoint_prior_scale = st.slider('Flexibility of Trend', 0.1, 10.0, 0.5, 0.1, format="%.1f")
 
-def analyze_sentiment(text):
-    blob = TextBlob(text.lower())
-    sentiment_score = 0
+        # Daily data for exponential smoothing
+        data['Date'] = pd.to_datetime(data['Date'])
+        data.set_index('Date', inplace=True)
+        daily_data = data.resample('D').interpolate()
+        daily_data['Close_rolling'] = daily_data['Close'].ewm(alpha=1 - smoothing_factor).mean()
 
-    for word in blob.words:
-        if word in positive_words:
-            sentiment_score += 1
-        elif word in negative_words:
-            sentiment_score -= 1
+        def plot_raw_data():
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Open'], name="Stock Open"))
+            fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close'], name="Stock Close"))
+            fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
+            fig.update_layout(
+                title_text='Stock History',
+                xaxis_rangeslider_visible=True,
+                height=600,
+                width=900
+            )
+            st.plotly_chart(fig)
 
-    return sentiment_score
+        plot_raw_data()
 
-if selected_stock:
-    data_load_state = st.text('Loading data...')
-    data = load_data(selected_stock)
-    data_load_state.text('Loading data... done!')
+        news_data = get_news(selected_stock)
 
-    smoothing_factor = st.slider('Smoothing Factor (increase for smoother graph)', 0.1, 0.95, 0.9, 0.05)
-    changepoint_prior_scale = st.slider('Flexibility of Trend', 0.1, 10.0, 0.5, 0.1, format="%.1f")
+        overall_sentiment_score = 0
+        if 'articles' in news_data and len(news_data['articles']) > 0:
+            for article in news_data['articles']:
+                # Check if the description has a minimum word count to consider it relevant
+                min_word_count = 10
+                if len(article['description'].split()) >= min_word_count:
+                    st.write(f"**URL:** {article['url']}")
+                    sentiment_score = analyze_sentiment(article['description'])
+                    weight = 10
+                    sentiment_score *= weight
+                    overall_sentiment_score += sentiment_score
 
-    # Daily data for exponential smoothing
-    data['Date'] = pd.to_datetime(data['Date'])
-    data.set_index('Date', inplace=True)
-    daily_data = data.resample('D').interpolate()
-    daily_data['Close_rolling'] = daily_data['Close'].ewm(alpha=1 - smoothing_factor).mean()
+        if overall_sentiment_score > 0:
+            st.subheader("Overall Sentiment: Positive")
+        elif overall_sentiment_score < 0:
+            st.subheader("Overall Sentiment: Negative")
+        else:
+            st.subheader("Overall Sentiment: Neutral")
 
-    def plot_raw_data():
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Open'], name="Stock Open"))
-        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close'], name="Stock Close"))
-        fig.add_trace(go.Scatter(x=daily_data.index, y=daily_data['Close_rolling'], name="Close (Exponential Smoothing)"))
-        fig.update_layout(
-            title_text='Stock History',
+        df_train = daily_data[['Close_rolling']].reset_index().rename(columns={"Date": "ds", "Close_rolling": "y"})
+
+        m = Prophet(
+            growth='linear',
+            changepoint_prior_scale=changepoint_prior_scale
+        )
+
+        m.fit(df_train)
+
+        future = m.make_future_dataframe(periods=period, freq='D')
+
+        forecast = m.predict(future)
+
+        if n_years == 1:
+            st.subheader(f'Forecast Plot for {n_years} Year')
+        else:
+            st.subheader(f'Forecast Plot for {n_years} Years')
+
+        fig1 = plot_plotly(m, forecast)
+
+        fig1.update_traces(mode='lines', line=dict(color='blue', width=2), selector=dict(name='yhat'))
+
+        num_data_points = len(forecast)
+        marker_size = max(4, 200 // num_data_points)
+
+        fig1.update_traces(mode='markers+lines', marker=dict(size=marker_size, color='black', opacity=0.7),
+                           selector=dict(name='yhat_lower,yhat_upper'))
+
+        fig1.update_layout(
+            title_text=f'Forecast Plot for {n_years} Years',
             xaxis_rangeslider_visible=True,
             height=600,
-            width=900
+            width=900,
+            legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1
+            )
         )
-        st.plotly_chart(fig)
 
-    plot_raw_data()
+        st.plotly_chart(fig1)
 
-    news_data = get_news(selected_stock)
+elif choice == "Compare Stocks":
+    selected_stocks = st.multiselect('Select stock tickers for comparison (refer to yfinance for tickers)', 
+                                     ['AAPL', 'MSFT', 'GOOGL', 'AMZN'])
 
-    overall_sentiment_score = 0
-    if 'articles' in news_data and len(news_data['articles']) > 0:
-        for article in news_data['articles']:
-            # Check if the description has a minimum word count to consider it relevant
-            min_word_count = 10  # Experiment with different values here
-            if len(article['description'].split()) >= min_word_count:
-                st.write(f"**URL:** {article['url']}")
+    if selected_stocks:
+        data_load_state = st.text('Loading data...')
+        data = pd.DataFrame()
+        for stock in selected_stocks:
+            stock_data = load_data(stock)
+            stock_data['Stock'] = stock
+            data = pd.concat([data, stock_data], ignore_index=True)
+        data_load_state.text('Loading data... done!')
+        
+        def plot_comparison():
+            fig = go.Figure()
+            for stock in selected_stocks:
+                stock_data = data[data['Stock'] == stock]
+                fig.add_trace(go.Scatter(x=stock_data['Date'], y=stock_data['Close'], name=stock))
+            
+            fig.update_layout(
+                title_text='Stock Comparison',
+                xaxis_rangeslider_visible=True,
+                height=600,
+                width=900
+            )
+            st.plotly_chart(fig)
 
-                sentiment_score = analyze_sentiment(article['description'])
-
-                # Adjust sensitivity by multiplying with a weight
-                weight = 10  # Experiment with different values here for sensitivity
-                sentiment_score *= weight
-
-                overall_sentiment_score += sentiment_score
-
-    if overall_sentiment_score > 0:
-        st.subheader("Overall Sentiment: Positive")
-    elif overall_sentiment_score < 0:
-        st.subheader("Overall Sentiment: Negative")
-    else:
-        st.subheader("Overall Sentiment: Neutral")
-
-    df_train = daily_data[['Close_rolling']].reset_index().rename(columns={"Date": "ds", "Close_rolling": "y"})
-
-    m = Prophet(
-        growth='linear',
-        changepoint_prior_scale=changepoint_prior_scale
-    )
-
-    m.fit(df_train)
-
-    future = m.make_future_dataframe(periods=period, freq='D')
-
-    forecast = m.predict(future)
-
-    if n_years == 1:
-        st.subheader(f'Forecast Plot for {n_years} Year')
-    else:
-        st.subheader(f'Forecast Plot for {n_years} Years')
-
-    fig1 = plot_plotly(m, forecast)
-
-    fig1.update_traces(mode='lines', line=dict(color='blue', width=2), selector=dict(name='yhat'))
-
-    num_data_points = len(forecast)
-    marker_size = max(4, 200 // num_data_points)
-
-    fig1.update_traces(mode='markers+lines', marker=dict(size=marker_size, color='black', opacity=0.7),
-                       selector=dict(name='yhat_lower,yhat_upper'))
-
-    fig1.update_layout(
-        title_text=f'Forecast Plot for {n_years} Years',
-        xaxis_rangeslider_visible=True,
-        height=600,
-        width=900,
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1
-        )
-    )
-
-    st.plotly_chart(fig1)
+        plot_comparison()
 
 footer = """
 <style>
